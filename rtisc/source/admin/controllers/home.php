e7fffe28dfe14b35c59b00270f7fdf3c6edce128
roushank <roushankumar1902@gmail.com>
2023-07-02 18:17:31 +0530

    Delete rtisc/source directory

--- rtisc/source/admin/controllers/home.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Home extends Controller{
-	protected function Index(){
-		$viewmodel = new HomeModel();
-		$this->ReturnView($viewmodel->Index(), true);
-	}
-}
--- rtisc/source/admin/models/transcation.php
+++
@@ -1,8 +0,0 @@
-<?php
-class TranscationModel extends Model{
-    public function Index(){
-        $this->query('Select * FROM orders WHERE ystatus = "Transaction authorised successfully" ORDER BY id DESC');
-        $rows = $this->resultSet();
-        return $rows;
-    }
-}
