fb1725dc1ed4593fdc8d737498abe01827b55a55
roushank <roushankumar1902@gmail.com>
2023-07-05 17:20:53 +0530

    Update edit.php

--- member/edit.php
+++ member/edit.php
@@ -1,38 +1,8 @@
-<?php include('inc/header.php'); ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <link rel="stylesheet" href="style.css">
-
-    <link rel="icon" href="Favicon.png">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
-    <title>Member Update Data</title>
-</head>
-<body>
-
-
-<!-- Header: I have to fetch the username here... It will fetch all the data of this ID -->
-<?php
+<?php include('inc/header.php');
 // Check if the ID parameter exists in the URL
-if (isset($_GET['userid'])) {
+if (isset( $_SESSION['id'])) {
     // Retrieve the ID from the URL
-    $id = $_GET['userid'];
-
+    $id = $_SESSION['id'];
     // Set the value of the text field
     $textFieldValue = $id;
 } else {
@@ -49,33 +19,24 @@ if (isset($_GET['userid'])) {
         <div class="col-md-7">
             <div class="card mt-5">
                 <div class="card-body">
-                    <form method="GET" action="">
-                        <div class="row">
-                            <div class="col-md-8">
-                                <input type="text" name="userid" value="<?php echo $textFieldValue; ?>" readonly class="form-control">
-                            </div>
-                            <div class="col-md-4">
-                                <button type="submit" name="details" class="btn btn-primary">User Details</button>
-                            </div>
-                        </div>
-                    </form>
 
                     <div class="row">
                         <div class="col-md-12">
                             <hr>
                             <?php
-                            $con = mysqli_connect("localhost", "root", "", "scms");
+                                $id = $_SESSION['id'];
 
-                            if (isset($_GET['details'])) {
-                                $userid = $_GET['userid'];
-
-                                $query = "SELECT users.*, address.* FROM users JOIN address ON users.userid = address.id WHERE users.userid='$userid' ";
+                                $query = "SELECT users.*, address.* FROM users JOIN address ON users.userid = address.id WHERE users.id='$id' ";
                                 $query_run = mysqli_query($con, $query);
 
                                 if (mysqli_num_rows($query_run) > 0) {
                                     foreach ($query_run as $row) {
                                         ?>
                                         <form method="POST" action="">
+                                        <div class="form-group mb-3">
+                                                <label for="">Member ID</label>
+                                                <input type="text" name="memberid" value="<?= $row['userid']; ?>" readonly class="form-control">
+                                            </div>
                                             <div class="form-group mb-3">
                                                 <label for="">Name</label>
                                                 <input type="text" name="username" value="<?= $row['username']; ?>" class="form-control">
@@ -115,7 +76,7 @@ if (isset($_GET['userid'])) {
                                 } else {
                                     echo "No Record Found";
                                 }
-                            }
+                            
                             ?>
                         </div>
                     </div>
@@ -146,7 +107,7 @@ if (isset($_POST['update'])) {
                   address.state = '$state',
                   address.city = '$city',
                   address.zipcode = '$zipcode'
-              WHERE users.userid = '$userid'";
+              WHERE users.id = '$id'";
     $query_run = mysqli_query($con, $query);
 
     if ($query_run) {
@@ -155,14 +116,5 @@ if (isset($_POST['update'])) {
         echo "Something went wrong";
     }
 }
-?>
-<!-----udpated---->
 
-
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
-
-<?php include('inc/footer.php'); ?>
+include('inc/footer.php'); ?>
